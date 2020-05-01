7580ed1f91689409594d1c6a625cc26598c509f3
simonaspa <akimirksnis@gmail.com>
2020-05-01 17:58:23 +0300

    update example

--- ArgyleExample/ArgyleExample/ViewController.swift
+++ ArgyleExample/ArgyleExample/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 import Argyle
 
 class ViewController: UIViewController {
-    
+
     let EXISTING_USER_TOKEN_KEY = "EXISTING_USER_TOKEN_KEY"
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         _ = Argyle.shared
@@ -21,13 +21,13 @@ class ViewController: UIViewController {
             //.dataPartners(["amazon_flex", "uber"])
             .resultListener(self)
     }
-    
+
     @IBAction func argyleNewUser(_ sender: Any) {
         let argyle = Argyle.shared.updateToken("").controller
         argyle.modalPresentationStyle = .fullScreen
         self.present(argyle, animated: true, completion: nil)
     }
-    
+
     @IBAction func argyleExistingUser(_ sender: Any) {
         if let token = UserDefaults.standard.value(forKey: EXISTING_USER_TOKEN_KEY) as? String {
             let argyle = Argyle.shared.updateToken(token).controller
@@ -37,52 +37,60 @@ class ViewController: UIViewController {
             showNoExistingToken()
         }
     }
-    
+
     func showNoExistingToken() {
         let alert = UIAlertController(title: "Error!", message: "No stored user token found.", preferredStyle: .alert)
-        
+
         alert.addAction(UIAlertAction(title: "OK", style: .cancel , handler:{ (UIAlertAction)in
-            
+
         }))
-        
+
         if let popoverController = alert.popoverPresentationController {
             popoverController.sourceRect = CGRect(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2, width: 0, height: 0)
             popoverController.sourceView = self.view
             popoverController.permittedArrowDirections = UIPopoverArrowDirection(rawValue: 0)
         }
-        
+
         self.present(alert, animated: true, completion: nil)
     }
-    
+
 }
 
 extension ViewController: ArgyleResultListener {
-    
-    func onAccountConnected(accountId: String, userId: String) {
-        print("APP: onAccountConnected(accountId: \(accountId), userId: \(userId))")
+
+    func onAccountCreated(accountId: String, userId: String, dataPartner:String) {
+        print("APP: onAccountCreated(accountId: \(accountId), userId: \(userId), dataPartner \(dataPartner))")
     }
-    
-    func onAccountRemoved(accountId: String, userId: String) {
-        print("APP: onAccountRemoved(accountId: \(accountId), userId: \(userId))")
+
+    func onAccountConnected(accountId: String, userId: String, dataPartner:String) {
+        print("APP: onAccountConnected(accountId: \(accountId), userId: \(userId), dataPartner \(dataPartner))")
     }
-    
+
+    func onAccountUpdated(accountId: String, userId: String, dataPartner:String) {
+        print("APP: onAccountUpdated(accountId: \(accountId), userId: \(userId), dataPartner \(dataPartner))")
+    }
+
+    func onAccountRemoved(accountId: String, userId: String, dataPartner:String) {
+        print("APP: onAccountRemoved(accountId: \(accountId), userId: \(userId), dataPartner \(dataPartner))")
+    }
+
     func onUserCreated(token: String, userId: String) {
         print("APP: onWorkerCreated((token: \(token), userId: \(userId))")
         UserDefaults.standard.set(token, forKey: EXISTING_USER_TOKEN_KEY)
     }
-    
+
     func onError(error: ArgyleErrorType) {
         print("APP: onError(error: \(error.rawValue))")
     }
-    
+
     func onTokenExpired(handler: @escaping (String) -> ()) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             handler("New token")
         }
     }
-    
+
     func onClose() {
         print("APP: onClose")
     }
-    
+
 }

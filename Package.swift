c64f11442fa986f8ee4f0c3815c01b1225de9e20
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2022-06-20 15:37:14 +0300

    Bump version of swift PM build

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ArgyleLink",
-            url: "https://github.com/argyle-systems/argyle-link-ios/releases/download/4.2.2-alpha1/ArgyleLink.xcframework.zip",
+            url: "https://github.com/argyle-systems/argyle-link-ios/releases/download/4.2.2-alpha3/ArgyleLink.xcframework.zip",
             checksum: "26c58d656c157e6207fa80ea1ec07a188aaea39f3ba79f8aa4188e70b294f892"
         )
     ]

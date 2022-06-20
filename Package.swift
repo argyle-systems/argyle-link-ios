a5f9e6599f5f1d109330645124c75d47db66cf15
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2022-06-20 15:34:18 +0300

    Fix swift PM manifest file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -8,13 +7,13 @@ let package = Package(
         .library(
             name: "ArgyleLink",
             targets: ["ArgyleLink"]
-        ),
+        )
     ],
     targets: [
         .binaryTarget(
             name: "ArgyleLink",
             url: "https://github.com/argyle-systems/argyle-link-ios/releases/download/4.2.2-alpha1/ArgyleLink.xcframework.zip",
             checksum: "26c58d656c157e6207fa80ea1ec07a188aaea39f3ba79f8aa4188e70b294f892"
-        ),
+        )
     ]
 )

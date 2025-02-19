7fc8fa7ba62dd92f661b31872f483e2914126910
Argyle Automation <argyle@argyle.com>
2025-02-19 09:00:29 +0000

    Bump SPM package version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "Argyle",
             url: "https://github.com/argyle-systems/argyle-link-ios/releases/download/5.13.0/Argyle.xcframework.zip",
-            checksum: "014d956dab588fce4cf7659db02f1ce72f126ea65491d8efa12c78968cfa366e"
+            checksum: "a4b2168b133646b34c03dbd66982227f984915ce777956a1b7c8e49bdb784418"
         )
     ]
 )
